322e121a087f725ba21d8a9d4c1f8b695aa7ed99
Anastasiia Karpova <200856629@yoobeestudent.ac.nz>
2021-10-12 09:46:43 +1300

    Line function Added

--- Hangman_Lab1/Hangman_Lab1.cpp
+++ Hangman_Lab1/Hangman_Lab1.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
 using namespace std;
 
+//prototypes
 void DrawHangman();
 void InvertHangMan();
 void WalkMan();
 char Play();
+void line(int dashes);
 
 int main()
 {
-	char realLetter = 'o';
-	string answer;
+	int dashes = 66;
+	string answer;//declaring the varible for the play function
+	cout << "\nWelcome to play HangMan. This game is to check your spelling skill." << endl;
+	line(dashes);//calling function for lines void type
+	cout << endl;
+	DrawHangman();//calling function for Hangman void type
 	cout << "\nGuess the missing letter in Yo_bee: ";
-	answer = Play();
+	answer = Play();//) calling function and Assign the return value of the function
 }
 
-void DrawHangman()
+
+
+void line(int dashes)//declaring function for line
+{
+	for (int i = 0; i < dashes; i++)
+	{
+		cout << "-";
+	}
+}
+
+void DrawHangman()//declaring function
 {
 	cout << "   ___ " << endl;
 	cout << "  |   |" << endl;
@@ -27,7 +43,7 @@ void DrawHangman()
 	cout << "-----" << endl;
 }
 
-void InvertHangMan()
+void InvertHangMan()//declaring function
 {
 	cout << "   ___ " << endl;
 	cout << "  |   |" << endl;
@@ -40,7 +56,7 @@ void InvertHangMan()
 	cout << "-----" << endl;
 }
 
-void WalkMan()
+void WalkMan()//declaring function
 {
 	cout << "     o" << endl;
 	cout << "    \\|/" << endl;
@@ -49,22 +65,22 @@ void WalkMan()
 }
 
 
-char Play()
+char Play()//function for playing return type
 {
-	char guessLetter, realLetter;
+	char guessLetter, realLetter;//declarung 2 varibles
 	realLetter = 'o';
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 3; i++)//creating for loop in order for game not to stop after one guess
 	{
-		cin >> guessLetter;
-		if (guessLetter == realLetter)
+		cin >> guessLetter;//asking user for input
+		if (guessLetter == realLetter)//checking condition 
 		{
 			cout << "\nYour guess is correct!" << endl;
-			WalkMan();
-			break;
+			WalkMan();//calling function for walkman(guess is correct)
+			break;//esiting the loop 
 		}
 		else
 		{
-			if (i < 2)
+			if (i < 2)//there are two output for wrong guess. 1) Ask user to try again 2) end game after third wrong guess
 			{
 				cout << "\nSorry, Try again..." << endl;
 			}
@@ -75,5 +91,5 @@ char Play()
 			}
 		}
 	}
-	return(guessLetter);
-}
+	return(guessLetter);//returning the value of the guess number
+}s
